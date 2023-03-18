44005059a1318da469fc03963d7e5268611b0412
Justin Nguyen <justinnguyen2245@gmail.com>
2023-03-17 21:14:11 -0700

    Getter and setter methods

--- src/main/java/Manufacturer.java
+++ src/main/java/Manufacturer.java
@@ -24,4 +24,47 @@ public class Manufacturer {
         this.phoneNumber = phoneNumber;
         this.address = address;
     }
+
+    /**
+     * @param name Name of manufacturer you want to set
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    /**
+     * @param phoneNumber Phone number of manufacturer you want to set
+     */
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @param address Address of manufacturer you want to set
+     */
+    public void setAddress(Address address){
+        this.address = address;
+    }
+
+    /**
+     * @return Name of manufacturer
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * @return Phone number of manufacturer
+     */
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    /**
+     * @return Address of manufacturer
+     */
+    public Address getAddress(){
+        return address;
+    }
+
 }
