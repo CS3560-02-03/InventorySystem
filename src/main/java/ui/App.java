8e55aed4e11e462f4fe0052ed7abe2eed18d172a
NickTriance <114621135+NickTriance@users.noreply.github.com>
2023-04-15 20:57:56 -0700

    UI Framework
    prolly gonna have to rewrite this in vuejs or something lol

--- src/main/java/ui/App.java
+++ src/main/java/ui/App.java
@@ -1,6 +1,8 @@
 package main.java.ui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 
 import main.java.ui.resource.AppStrings;
 import main.java.ui.resource.AppColors;
@@ -14,6 +16,10 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(640,480);
         frame.setTitle(AppStrings.APP_TITLE);
+
+        //set application icon
+        ImageIcon icon = new ImageIcon(".\\src\\main\\java\\ui\\resource\\img\\appicon.png");
+        frame.setIconImage(icon.getImage());        
 
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(AppColors.MAIN_BG);
--- src/main/java/ui/resource/AppColors.java
+++ src/main/java/ui/resource/AppColors.java
@@ -4,6 +4,14 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.color.*;;
 public final class AppColors {
-    public static final Color MAIN_BG = Color.BLUE;
-    public static final Color MAIN_FONT = Color.WHITE;
+
+    //Main Palette
+    public static final Color WHITE = new Color(180, 180, 180);
+    public static final Color LIGHT_BLUE = new Color(0,168,208);
+    public static final Color DARK_BLUE = new Color(0,106,208);
+    public static final Color BLACK = new Color(0,0,0);
+
+    //Type Colors
+    public static final Color MAIN_BG = AppColors.DARK_BLUE;
+    public static final Color MAIN_FONT = AppColors.WHITE;
 }
