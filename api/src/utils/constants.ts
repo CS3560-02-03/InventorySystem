6e875f6cd19caf381aec81b56d8ab3c62448d4cf
bluesimp1102 <jackismyname7@gmail.com>
2023-04-17 21:50:51 -0700

    products entities

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -1,9 +1,11 @@
 export enum ROUTES {
     ACCOUNT = 'accounts',
+    PRODUCT = 'products'
 }
   
 export enum SERVICES {
     ACCOUNT = 'ACCOUNT_SERVICE',
+    PRODUCT = 'ACCOUNT_SERVICE',
 }
 
 export enum BASIC_SERVICE_ACTIONS {
