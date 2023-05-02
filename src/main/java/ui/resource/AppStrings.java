2e173b840e91313efe612ad746139f14426f3aa8
NickTriance <114621135+NickTriance@users.noreply.github.com>
2023-05-01 21:47:00 -0700

    Removed unused Java code

--- src/main/java/ui/resource/AppStrings.java
+++
@@ -1,7 +0,0 @@
-package main.java.ui.resource;
-
-/**Class for holding all of the strings in the UI */
-public final class AppStrings {
-    public static final String APP_TITLE = "Dunder Mifflin Inventory System";
-
-}
--- src/main/java/utils/ShipmentStatus.java
+++
@@ -1,8 +0,0 @@
-package main.java.utils;
-
-/**States a shipment can be in */
-public enum ShipmentStatus {
-    PROCESSING,
-    SHIPPED,
-    DELIVERED
-}
